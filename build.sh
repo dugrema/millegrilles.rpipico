be23438dd7f1581561e98a2900e444ce382d2459
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-11-21 15:46:17 -0500

    Copie build vers top rep

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 REP_BASE=`pwd`
+REP_RP2="${REP_BASE}/micropython/ports/rp2"
 
-cd "${REP_BASE}/micropython/ports/rp2"
+export BOARD=PICO_W
 
-make clean BOARD=PICO_W
-make BOARD=PICO_W
+rm "${REP_BASE}/firmware.uf2"
 
+cd "$REP_RP2"
+
+make clean
+make
+
+#make clean BOARD=${BOARD}
+#make BOARD=${BOARD}
+
+cp "${REP_BASE}/micropython/ports/rp2/build-PICO_W/firmware.uf2" "${REP_BASE}"
