5b33c094c4b78f7fd29353a7d4261abdb5c10bbf
Mathieu Dugre <mathieu.dugre@mdugre.info>
2023-01-05 08:15:41 -0500

    Fix build pour mpy deps

--- build.sh
+++ build.sh
@@ -3,12 +3,25 @@
 REP_BASE=`pwd`
 REP_RP2="${REP_BASE}/micropython/ports/rp2"
 REP_MG_SRC="${REP_BASE}/millegrilles/src"
+REP_MILLEGRILLES_PYTHON="${REP_BASE}/millegrilles/python"
+REP_MILLEGRILLES_LIB="${REP_BASE}/millegrilles/lib"
 
 export BOARD=PICO_W
 export USER_C_MODULES="${REP_MG_SRC}/micropython.cmake"
 
 rm "${REP_BASE}/firmware.uf2"
 
+# Preparer mpy PYTHON
+cd "${REP_MILLEGRILLES_PYTHON}"
+make clean; make all
+cp -r "${REP_MILLEGRILLES_PYTHON}/build/millegrilles" "${REP_RP2}/modules/"
+
+# Prepary mpy libs
+cd "${REP_MILLEGRILLES_LIB}"
+make clean; make all
+cp -r ${REP_MILLEGRILLES_LIB}/build/* "${REP_RP2}/modules/"
+
+# Move dans rep RP2
 cd "$REP_RP2"
 
 if [ -z $NOCLEAN ]; then
