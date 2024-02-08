c47fd3e5d5170302c1822a77929470774b787120
mathieu <mathieu.dugre@mdugre.info>
2024-02-08 08:03:03 -0500

    Ajout rep build, set version

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 REP_BASE=`pwd`
+REP_BUILD="${REP_BASE}/build"
 REP_RP2="${REP_BASE}/micropython/ports/rp2"
 REP_MG_SRC="${REP_BASE}/millegrilles/src"
 REP_MILLEGRILLES_PYTHON="${REP_BASE}/millegrilles/python"
@@ -9,9 +10,11 @@ REP_MILLEGRILLES_LIB="${REP_BASE}/millegrilles/lib"
 export BOARD=RPI_PICO_W
 export USER_C_MODULES="${REP_MG_SRC}/micropython.cmake"
 
-rm "${REP_BASE}/firmware.uf2"
+mkdir -p build
+rm "${REP_BUILD}/firmware.uf2"
 
 # Preparer mpy PYTHON
+echo "MILLEGRILLES_VERSION=const('2024.0.5')" > ${REP_MILLEGRILLES_PYTHON}/millegrilles/version.py
 cd "${REP_MILLEGRILLES_PYTHON}"
 make clean; make all
 cp -r "${REP_MILLEGRILLES_PYTHON}/build/millegrilles" "${REP_RP2}/modules/"
@@ -30,4 +33,4 @@ if [ -z $NOCLEAN ]; then
 fi
 make
 
-cp "${REP_BASE}/micropython/ports/rp2/build-${BOARD}/firmware.uf2" "${REP_BASE}"
+cp "${REP_BASE}/micropython/ports/rp2/build-${BOARD}/firmware.uf2" "${REP_BUILD}"
